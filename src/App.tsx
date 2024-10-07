3ec149aab44160451109e417f7ee1d5f20d7d9d0
Jeffrey Monja Castro <104637993+jeffreymonjacastro@users.noreply.github.com>
2024-10-07 18:25:17 -0500

    initial config react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import CourseList from './components/CourseList'
+import Register from './components/Register'
+
+function App() {
+
+  return (
+    <>
+      <Register />
+      <CourseList />
+    </>
+  )
+}
+
+export default App
+
